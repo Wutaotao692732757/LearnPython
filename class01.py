478213a924755bd9f2b104c8aee8744ea69ce57e
Wutaotao692732757 <692732757@qq.com>
2017-06-30 10:56:50 +0800

    class01

---
+++ class01.py
@@ -0,0 +1,39 @@
+  #!/usr/bin/python
+  # _*_ coding: UTF-8 _*_
+
+
+if True:
+      print "True"
+else:
+      print "False"
+
+days = ['Monday','Tuesday','Wednesday','Thursday','Friday']
+
+total = 1 + \
+        2 + \
+        3
+
+print total
+
+word = 'word'
+sentence = "这是一个句子."
+paragraph = """这是一个段落.包含了多个语句"""
+# 第一个注释
+print word ; #第二个注释
+
+'''这个是单行注释'''
+
+import sys; x = 'runoob'; sys.stdout.write(x + '\n')
+
+x = "a"
+y = "b"
+# 换行输出
+print x
+print y
+
+print '------------'
+# 不换行输出
+print x,
+print y,
+
+
